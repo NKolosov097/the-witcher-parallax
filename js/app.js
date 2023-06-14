eb0102a3963397cf858cd1419c9df953c462f2fb
Kolosov Nikita <87139155+NKolosov097@users.noreply.github.com>
2023-06-14 22:30:12 +0300

    init:  the-witcher-parallax project

---
+++ js/app.js
@@ -0,0 +1,11 @@
+new Swiper(".slider", {
+  //   direction: "vertical",
+  //   freeMode: true,
+  speed: 2400,
+  mousewheel: {
+    enabled: true,
+    sensitivity: 2.7,
+  },
+  spaceBetween: 18,
+  parallax: true,
+});
